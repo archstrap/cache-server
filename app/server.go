f50569d267eba5fe6891cd7a7f74c2d30d563bb0
archstrap <atishnaskar1269@gmail.com>
2025-02-17 03:47:44 +0530

    [ Add | multiple clients support ]

--- app/server.go
+++ app/server.go
@@ -2,37 +2,63 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	host := "0.0.0.0"
+	port := "6379"
 
-	// Uncomment this block to pass the first stage
+	serverAddress := fmt.Sprintf("%s:%s", host, port)
+	l := startServerAndReturnConnection(serverAddress)
+	handleServerRequest(l)
+}
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+func startServerAndReturnConnection(serverAddress string) net.Listener {
+	listener, err := net.Listen("tcp", serverAddress)
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	fmt.Printf("server started at %s\n", serverAddress)
+	return listener
+}
+
+func handleServerRequest(l net.Listener) {
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		handleConnection(conn)
+		go handleConnection(conn)
 	}
 }
 
 func handleConnection(conn net.Conn) {
-	message := []byte("+PONG\r\n")
-	conn.Write(message)
+
+	fmt.Printf("Handling client: %v\n", conn.RemoteAddr())
+
+	for {
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+
+		if err != nil {
+			fmt.Println("Error", err)
+			conn.Close()
+			fmt.Printf("Connection from: %s is closed\n", conn.RemoteAddr())
+			if err == io.EOF {
+				break
+			}
+		}
+
+		receivedMessage := string(buf[:n])
+		fmt.Printf("client address:%s , sent message:%s \n", conn.RemoteAddr(), receivedMessage)
+
+		message := []byte("+PONG\r\n")
+		conn.Write(message)
+	}
 
 }
