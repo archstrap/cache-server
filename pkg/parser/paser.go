a688791da569f5c2787e94c624b7f0132703f5cf
archstrap <atishnaskar1269@gmail.com>
2025-12-02 03:55:28 +0530

    Add parser for Bulk String and array

--- pkg/parser/paser.go
+++ pkg/parser/paser.go
@@ -15,6 +15,7 @@ const (
 	TypeArray        RespType = '*'
 	TypeInteger      RespType = ':'
 	TypeSimpleString RespType = '+'
+	TypeBulkString   RespType = '$'
 )
 
 type RespValue struct {
@@ -35,11 +36,24 @@ func Parse(ioReader io.Reader) (*RespValue, error) {
 
 	switch RespType(typeByte) {
 	case TypeArray:
+		data, err := parseArray(reader)
+		return &RespValue{
+			Value:    data,
+			DataType: TypeArray,
+		}, err
+
 	case TypeSimpleString:
 		simpleString, err := parseSimpleString(reader)
 		return &RespValue{
 			Value:    simpleString,
 			DataType: TypeSimpleString,
+		}, err
+
+	case TypeBulkString:
+		bulkString, err := parseBulkString(reader)
+		return &RespValue{
+			Value:    bulkString,
+			DataType: TypeBulkString,
 		}, err
 
 	case TypeInteger:
@@ -48,6 +62,9 @@ func Parse(ioReader io.Reader) (*RespValue, error) {
 			Value:    integer,
 			DataType: TypeInteger,
 		}, err
+	default:
+		value.DataType = TypeSimpleString
+		value.Value = "HELLO"
 	}
 
 	return value, nil
@@ -77,5 +94,39 @@ func parseSimpleString(reader *bufio.Reader) (string, error) {
 	}
 	data = strings.TrimSpace(data)
 	return data, nil
+}
+
+func parseBulkString(reader *bufio.Reader) (string, error) {
+	identifierByter, _ := reader.ReadByte()
+
+	if RespType(identifierByter) != TypeBulkString {
+		// TODO add error
+	}
+
+	lengthOfTheString, _ := reader.ReadString('\n')
+	length, _ := strconv.Atoi(lengthOfTheString)
+
+	data, _ := reader.ReadString('\n')
+	data = strings.TrimSpace(data)
+
+	if length != len(data) {
+		// TODO
+	}
+
+	return data, nil
+
+}
+
+func parseArray(reader *bufio.Reader) ([]string, error) {
+
+	noOfElements, _ := parseInteger(reader)
+	elements := make([]string, noOfElements)
+
+	for i := 0; i < noOfElements; i++ {
+		data, _ := parseBulkString(reader)
+		elements[i] = data
+	}
+
+	return elements, nil
 
 }
