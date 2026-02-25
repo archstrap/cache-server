5cc57bc3938c908c07bb90391e0346ea5456148e
archstrap <atishnaskar1269@gmail.com>
2026-02-26 03:55:14 +0530

    Add Wait with no commands

--- internal/command/wait.go
+++ internal/command/wait.go
@@ -1,13 +1,34 @@
 package command
 
-import "github.com/archstrap/cache-server/pkg/model"
+import (
+	"strconv"
+
+	"github.com/archstrap/cache-server/internal/replication"
+	"github.com/archstrap/cache-server/pkg/model"
+)
 
 type WaitCommand struct{}
 
 var WaitCommandInstance *WaitCommand = &WaitCommand{}
 
 func (command *WaitCommand) Process(input *model.RespValue) *model.RespOutput {
-	return model.NewRespOutput(model.TypeInteger, 0)
+
+	args := input.ArgsToStringSlice()
+
+	if len(args) != 3 {
+		return model.NewWrongNumberOfOutput(command.Name())
+	}
+
+	noOfReplicas, err := strconv.Atoi(args[1])
+	if err != nil {
+		return model.NewRespOutput(model.TypeError, err)
+	}
+
+	if noOfReplicas == 0 {
+		return model.NewRespOutput(model.TypeInteger, 0)
+	}
+
+	return model.NewRespOutput(model.TypeInteger, replication.GetReplicationStore().ActiveReplicationCount())
 }
 
 func (command *WaitCommand) Name() string {
