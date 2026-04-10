aa48332227b1ec2c5227e9ba4fbf1f2c9ef2dffc
archstrap <atishnaskar1269@gmail.com>
2026-04-11 04:20:53 +0530

    Add the GEOADD functionality

--- internal/command/geoadd.go
+++ internal/command/geoadd.go
@@ -3,6 +3,7 @@ package command
 import (
 	"fmt"
 
+	"github.com/archstrap/cache-server/internal/store"
 	"github.com/archstrap/cache-server/pkg/model"
 	"github.com/archstrap/cache-server/util"
 )
@@ -22,20 +23,27 @@ func (g *GEOADD) Process(value *model.RespValue) *model.RespOutput {
 		return err
 	}
 
-	lattitude, err := util.ParseFloat(args[3])
+	latitude, err := util.ParseFloat(args[3])
 	if err != nil {
 		return err
 	}
 
 	if longitude < -180 || longitude > 180 {
-		return model.NewRespOutput(model.TypeError, fmt.Sprintf("ERR invalid longitude,latitude pair %g,%g", longitude, lattitude))
+		return model.NewRespOutput(model.TypeError, fmt.Sprintf("ERR invalid longitude,latitude pair %g,%g", longitude, latitude))
 	}
 
-	if lattitude < -85.05112878 || lattitude > 85.05112878 {
-		return model.NewRespOutput(model.TypeError, fmt.Sprintf("ERR invalid longitude,latitude pair %g,%g", longitude, lattitude))
+	if latitude < -85.05112878 || latitude > 85.05112878 {
+		return model.NewRespOutput(model.TypeError, fmt.Sprintf("ERR invalid longitude,latitude pair %g,%g", longitude, latitude))
 	}
 
-	return model.NewRespOutput(model.TypeInteger, 1)
+	key := args[1]
+	score := fmt.Sprintf("%d", util.Encode(latitude, longitude))
+	member := args[4]
+
+	item := store.NewSetItem(key, score, member)
+	bucket := store.GetSkipListBucket()
+
+	return model.NewRespOutput(model.TypeInteger, bucket.Insert(item))
 }
 
 func init() {
